7326d380048a9ee5e6422b781bf6ae34d6146cfc
dobov <dobo.vivien@wysio.hu>
2022-05-25 08:34:57 +0200

    ExceptionMessage enum kiegészítése HttpStatus típusú adattaggal
    Az ExceptionMessage már eltárolja a hibaüzenetet és a hozzá tartozó http státusz kódját is. Emellett felvételre került egy ISMERETLEN_HIBA is, amelyet a nem WysioKocsmaException típusú exceptionök dobásakor használunk.

--- src/main/java/hu/wysio/training/vivi/wysiokocsma/model/ExceptionMessage.java
+++ src/main/java/hu/wysio/training/vivi/wysiokocsma/model/ExceptionMessage.java
@@ -2,17 +2,20 @@ package hu.wysio.training.vivi.wysiokocsma.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
 public enum ExceptionMessage {
 
-	SIKERTELEN("Sikertelen művelet."),
-	NINCS_FOGYASZTAS("Nincs ilyen fogyasztás az alabbi id-val: "),
-	NINCS_ITAL("Nincs ilyen ital az alabbi id-val: "),
-	NINCS_VENDEG("Nincs ilyen vendég az alabbi id-val: "),
-	NINCS_KOCSMAZAS("Nincs ilyen kocsmázás az alabbi id-val: "),
-	NINCS_BUNYO("Nincs ilyen bunyó az alabbi id-val: ");
+	SIKERTELEN("Sikertelen művelet.", HttpStatus.INTERNAL_SERVER_ERROR),
+	NINCS_FOGYASZTAS("Nincs ilyen fogyasztás ezzel az id-val.", HttpStatus.NOT_FOUND),
+	NINCS_ITAL("Nincs ilyen ital ezzel az id-val.", HttpStatus.NOT_FOUND),
+	NINCS_VENDEG("Nincs ilyen vendég ezzel az id-val.", HttpStatus.NOT_FOUND),
+	NINCS_KOCSMAZAS("Nincs ilyen kocsmázás ezzel az id-val.", HttpStatus.NOT_FOUND),
+	NINCS_BUNYO("Nincs ilyen bunyó ezzel az id-val.", HttpStatus.NOT_FOUND),
+	ISMERETLEN_HIBA("Ismeretlen hiba!", HttpStatus.INTERNAL_SERVER_ERROR);
 
 	private final String message;
+	private final HttpStatus httpStatus;
 }
