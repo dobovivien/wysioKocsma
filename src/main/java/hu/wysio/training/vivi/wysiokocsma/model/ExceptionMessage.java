e5e5039c1bba1c68f56e5e464eb35fcb39e254cb
dobov <dobo.vivien@wysio.hu>
2022-05-26 09:16:24 +0200

    Nem használt enum értékek törlése

--- src/main/java/hu/wysio/training/vivi/wysiokocsma/model/ExceptionMessage.java
+++ src/main/java/hu/wysio/training/vivi/wysiokocsma/model/ExceptionMessage.java
@@ -8,12 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionMessage {
 
-	SIKERTELEN("Sikertelen művelet.", HttpStatus.INTERNAL_SERVER_ERROR),
-	NINCS_FOGYASZTAS("Nincs ilyen fogyasztás ezzel az id-val.", HttpStatus.NOT_FOUND),
-	NINCS_ITAL("Nincs ilyen ital ezzel az id-val.", HttpStatus.NOT_FOUND),
-	NINCS_VENDEG("Nincs ilyen vendég ezzel az id-val.", HttpStatus.NOT_FOUND),
-	NINCS_KOCSMAZAS("Nincs ilyen kocsmázás ezzel az id-val.", HttpStatus.NOT_FOUND),
-	NINCS_BUNYO("Nincs ilyen bunyó ezzel az id-val.", HttpStatus.NOT_FOUND),
+	NINCS_FOGYASZTAS("Nincs ilyen fogyasztás a megadott id-val.", HttpStatus.NOT_FOUND),
+	NINCS_ITAL("Nincs ilyen ital a megadott id-val.", HttpStatus.NOT_FOUND),
+	NINCS_VENDEG("Nincs ilyen vendég a megadott id-val.", HttpStatus.NOT_FOUND),
 	ISMERETLEN_HIBA("Ismeretlen hiba!", HttpStatus.INTERNAL_SERVER_ERROR);
 
 	private final String message;
