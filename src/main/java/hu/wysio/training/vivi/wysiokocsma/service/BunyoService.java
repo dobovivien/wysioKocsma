8d2ebd24684239de7628131879f34cc6a13dbcf6
dobov <dobo.vivien@wysio.hu>
2022-05-27 10:49:02 +0200

    freEach átírása streammé

--- src/main/java/hu/wysio/training/vivi/wysiokocsma/service/BunyoService.java
+++ src/main/java/hu/wysio/training/vivi/wysiokocsma/service/BunyoService.java
@@ -4,6 +4,7 @@ import hu.wysio.training.vivi.wysiokocsma.comparator.TabellaDtoComparator;
 import hu.wysio.training.vivi.wysiokocsma.converter.VendegConverter;
 import hu.wysio.training.vivi.wysiokocsma.dto.TabellaDto;
 import hu.wysio.training.vivi.wysiokocsma.model.Bunyo;
+import hu.wysio.training.vivi.wysiokocsma.model.Kocsmazas;
 import hu.wysio.training.vivi.wysiokocsma.model.Vendeg;
 import hu.wysio.training.vivi.wysiokocsma.repository.BunyoRepository;
 import hu.wysio.training.vivi.wysiokocsma.repository.VendegRepository;
@@ -11,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -35,13 +35,12 @@ public class BunyoService {
     private VendegConverter vendegConverter;
 
     public long startBunyo() {
-        Set<Vendeg> vendegSet = new HashSet<>();
-
         Bunyo bunyo = new Bunyo();
         bunyo.setMettol(LocalDateTime.now());
 
-        kocsmazasService.getAllBefejezetlenKocsmazas()
-                .forEach(kocsmazas -> vendegSet.add(kocsmazas.getVendeg()));
+        Set<Vendeg> vendegSet = kocsmazasService.getAllBefejezetlenKocsmazas().stream()
+                .map(Kocsmazas::getVendeg)
+                .collect(Collectors.toSet());
 
         bunyo.setVendegList(vendegSet);
 
